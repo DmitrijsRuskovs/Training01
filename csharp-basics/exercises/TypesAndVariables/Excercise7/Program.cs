019a20fef66e7d1c4af9c68d24ff4352c8b5f955
Dmitrijs Ruskovs <dmitrijs.ruskovs@gmail.com>
2021-07-14 11:05:14 +0300

    Excercise#7 complete

---
+++ csharp-basics/exercises/TypesAndVariables/Excercise7/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Excercise7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please, enter a word, Liva: ");
+            String word = Console.ReadLine();
+            String lowerWord = word.ToLower();
+            int sum = 0;
+            for (int i = 0; i < word.Length; i++)
+            {
+                if (word[i]!=lowerWord[i]) sum ++;
+            }
+            Console.WriteLine("There are " + sum.ToString()+" capitals in Your word, Liva");
+
+            Console.ReadKey();
+        }
+    }
+}
