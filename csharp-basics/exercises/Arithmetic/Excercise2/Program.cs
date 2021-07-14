6b41bb833bfc7be70749471cc5381c84f329c0bd
Dmitrijs Ruskovs <dmitrijs.ruskovs@gmail.com>
2021-07-14 14:53:06 +0300

    Excercise#2 complete

---
+++ csharp-basics/exercises/Arithmetic/Excercise2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CheckOddEven
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter number, Liva, please: ");
+            int number = Int32.Parse(Console.ReadLine());
+
+            if (number % 2 == 1) Console.WriteLine("Odd!");
+            else Console.WriteLine("Even!");
+            Console.WriteLine("Bye!");
+
+            Console.ReadKey();
+        }
+    }
+}
