bedaa43813a34e316f114e22abc03fb3b675701a
Dmitrijs Ruskovs <dmitrijs.ruskovs@gmail.com>
2021-07-15 22:37:57 +0300

    file count

---
+++ csharp-basics/exercises/TypesAndVariables/Excercise6/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Excercise6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("How many variables are You going to sum, Liva: ");
+            int count = Int32.Parse(Console.ReadLine());
+            int sum = 0;
+            for (int i = 0; i < count; i++)
+            {
+                Console.Write("Liva, enter number #"+(i+1).ToString()+":  ");
+                int number=Int32.Parse(Console.ReadLine());
+                sum += number;
+            }
+            Console.WriteLine("Sum of all Your numbers seems to be "+sum.ToString());
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ csharp-basics/exercises/TypesAndVariables/Excercise9/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Excercise9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter distance in meters, Liva: ");
+            long distance = Int64.Parse(Console.ReadLine());
+            Console.Write("Enter hours, Liva: ");
+            int hours = Int32.Parse(Console.ReadLine());
+            Console.Write("Enter minutes, Liva: ");
+            int minutes = Int32.Parse(Console.ReadLine());
+            Console.Write("Enter seconds, Liva: ");
+            int seconds = Int32.Parse(Console.ReadLine());
+            
+            
+            double speedMS = (double)distance / ((double)hours *60*60+ (double)minutes *60+ (double)seconds);
+            double speedKmH = (double)distance / 1000 / ((double)hours + (double)minutes /60 + (double)seconds /3600);
+            double speedMH = (double)distance / 1609 / ((double)hours + (double)minutes / 60 + (double)seconds / 3600);
+
+
+            Console.WriteLine("Your speed in meters / second is: " + String.Format("{0:0.00}", speedMS));
+            Console.WriteLine("Your speed in km / h is: " + String.Format("{0:0.00}", speedKmH));
+            Console.WriteLine("Your speed in miles / h is: " + String.Format("{0:0.00}", speedMH));
+           
+            Console.ReadKey();
+        
+        }
+    }
+}
