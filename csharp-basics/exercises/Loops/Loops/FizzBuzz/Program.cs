8ed55d5f582ca544f5d6a8aeef42f0d14c24fa39
Dmitrijs Ruskovs <dmitrijs.ruskovs@gmail.com>
2021-07-16 17:33:48 +0300

    Exercise#6 complete

---
+++ csharp-basics/exercises/Loops/Loops/FizzBuzz/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class FizzBuzz
+    {
+        static void Main(string[] args)
+        {
+            const int numbersPerLine = 20;
+            const int lowerBound = 1;
+            
+            Console.WriteLine("Enter number upto which to FizzBuzz?");
+            int higherBound = Convert.ToInt16(Console.ReadLine());
+
+            int currentIndex = lowerBound - 1;
+            while (currentIndex < higherBound + 1)
+            {
+                String numberSetLine = "";
+                for (int i = 1; i <= numbersPerLine; i++)
+                {
+                    currentIndex++;
+                    String currentLine = "";
+                    if (currentIndex > higherBound) break;
+                    if (currentIndex % 3 == 0) currentLine += "Fizz";
+                    if (currentIndex % 5 == 0) currentLine += "Buzz";                   
+                    if (currentLine.Length == 0) currentLine += currentIndex.ToString();
+                    numberSetLine += currentLine + " ";
+                }
+                Console.WriteLine(numberSetLine);
+            }
+            Console.ReadKey();
+        }
+    }
+}
