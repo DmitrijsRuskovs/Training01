dc108ddcdf3a9d81e5c264a66380e6d79f1ea1fc
Dmitrijs Ruskovs <dmitrijs.ruskovs@gmail.com>
2021-07-22 15:10:16 +0300

    Exercise#5 complete

--- csharp-basics/exercises/Collections/WordCount/Program.cs
+++ csharp-basics/exercises/Collections/WordCount/Program.cs
@@ -9,8 +9,33 @@ namespace WordCount
     class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+        {           
+            System.IO.StreamReader file = new System.IO.StreamReader(@"C:\Users\albin\source\repos\Training01\csharp-basics\exercises\Collections\WordCount\lear.txt");
+            
+            int lines = 0;
+            int words = 0;
+            int characters = 0;
+            string text = "";
+            while ((text = file.ReadLine()) != null)
+            {
+                lines++;
+                words++;
+                for (int i = 0; i < text.Length; i++)
+                {
+                    if (text[i] == ' '|| text[i] == (char)39)
+                    {
+                        words++;
+                    }
+                    
+                    if (Char.IsLetterOrDigit(text[i]))
+                    {
+                        characters++;
+                    }
+                }
+            }
+
+            Console.WriteLine($"Lines = {lines}; Words: {words}; Chars: {characters}");
+            Console.ReadKey();
         }
     }
 }
