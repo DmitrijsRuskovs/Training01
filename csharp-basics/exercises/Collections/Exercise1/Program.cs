04817654545a03a6a8bed33cd3cb1882449949b1
Dmitrijs Ruskovs <dmitrijs.ruskovs@gmail.com>
2021-07-22 12:33:05 +0300

    Exercise#1 complete

--- csharp-basics/exercises/Collections/Exercise1/Program.cs
+++ csharp-basics/exercises/Collections/Exercise1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -21,6 +22,34 @@ namespace Exercise1
         private static void Main(string[] args)
         {
             string[] array = { "Audi", "BMW", "Honda", "Mercedes", "VolksWagen", "Mercedes", "Tesla" };
+
+            List<string> myList = new List<string>() { "Audi", "BMW", "Honda", "Mercedes", "VolksWagen", "Mercedes", "Tesla" };
+            foreach (var i in myList)
+            {
+                Console.WriteLine(i);
+            }
+
+            Hashtable myHashTable = new Hashtable()
+            {
+                {"Audi","Germany"}, {"BMW","Germany"}, {"Honda","Japan"}, {"Mercedes","Germany"},{"VolksWagen","Germany"}, {"Tesla","USA"}
+            };
+
+            foreach (DictionaryEntry i in myHashTable)
+            {
+                Console.WriteLine(i.Key+" "+i.Value);
+            }
+
+            Dictionary<string,string> myDictionary= new Dictionary<string, string>()
+            {
+                {"Audi","Germany"}, {"BMW","Germany"}, {"Honda","Japan"}, {"Mercedes","Germany"},{"VolksWagen","Germany"}, {"Tesla","USA"}
+            };
+
+            foreach (KeyValuePair<string, string> i in myDictionary)
+            {
+                Console.WriteLine(i.Key + " " + i.Value);
+            }
+
+            Console.ReadKey();
 
             //todo - replace array with an List and print out the results
 
