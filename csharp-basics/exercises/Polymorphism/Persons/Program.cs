f599983ccb1fe09c5dc90b7b0c4e6c5b1dc6e1a3
Dmitrijs Ruskovs <dmitrijs.ruskovs@gmail.com>
2021-07-27 16:18:00 +0300

    Exercise#3 complete

--- csharp-basics/exercises/Polymorphism/Persons/Program.cs
+++ csharp-basics/exercises/Polymorphism/Persons/Program.cs
@@ -2,14 +2,17 @@
 
 namespace Persons
 {
+    
+
     class Program
     {
         static void Main(string[] args)
         {
-            /*
-             * - Create an instant of class Student and an instant of class Employee with proper values for the attributes.
-             * - Display the content of each class using Display() method.
-             */
+            Student Valera = new Student(6.7, "Valera", "Samokatov", "Strelnieku laukums 1");
+            Employee SanSanych = new Employee("Rain-stopper", "Aleksandrs", "Holodilnikov", "Strelnieku laukums 2");
+            Valera.Display();
+            SanSanych.Display();
+            Console.ReadKey();
         }
     }
 }
