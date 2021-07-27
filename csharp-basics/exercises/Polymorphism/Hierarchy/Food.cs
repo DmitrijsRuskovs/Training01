76e8851d83c33b356332f7b6e3706793e8878dd9
Dmitrijs Ruskovs <dmitrijs.ruskovs@gmail.com>
2021-07-27 22:48:12 +0300

    Exercise#6 complete

---
+++ csharp-basics/exercises/Polymorphism/Hierarchy/Food.cs
@@ -0,0 +1,60 @@
+﻿namespace Hierarchy
+{
+    public abstract class Food
+    {
+        protected int _quantity;
+
+        protected Food()
+        {
+            _quantity = 0;
+        }
+
+        protected Food(int quantity)
+        {
+            _quantity = quantity;
+        }
+
+        public int GetQuantity()
+        {
+            return this._quantity;
+        }
+
+        public abstract string FoodType();
+    }
+
+    public class Vegetable : Food
+    {
+        public Vegetable()
+        {
+            this._quantity = 0;
+        }
+
+        public Vegetable(int quantity)
+        {
+            this._quantity = quantity;
+        }
+
+        public override string FoodType()
+        {
+            return "Vegetable";
+        }
+}
+
+    public class Meat : Food
+    {
+        public Meat()
+        {
+            this._quantity = 0;
+        }
+
+        public Meat(int quantity)
+        {
+            this._quantity = quantity;
+        }
+
+        public override string FoodType()
+        {
+            return "Meat";
+        }
+    } 
+}
